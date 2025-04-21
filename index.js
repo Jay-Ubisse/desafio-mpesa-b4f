e779eaf150aa29f9921d5f991a438b29b4b38a09
bethynna2004 <dlambealbertina@gmail.com>
2025-04-21 15:48:33 +0200

    input option for mkesh

--- index.js
+++ index.js
@@ -16,39 +16,7 @@ do {
     7. Sair
     `)
   );
-<<<<<<< HEAD
 
-
-  switch
-  (option){
-  
-  case 1:
-  const númerodoreceptor = prompt ("Digita um numero valido");
-    if(
-      (númerodoreceptor.length === 9 && númerodoreceptor.charAt(1)==="4") ||
-      (númerodoreceptor.length === 9 && númerodoreceptor.charAt (1) === "5")
-    ){
-    
-      const transferirvalor = Numero (prompt ("Digita o valor a transferir"));
-  
-      if (transferirvalor){
-  
-      }
-         
-     } else {
-      saldo -= transferirvalor;
-  }
-      /*if (transferirvalor>0 && transferirvalor <= saldo){
-        alert (`transferencia de ${transferirvalor}5mt para ${númerodoreceptor} feita com sucesso`)
-    }
-  
-  
-  
-  
-  //porfavor introduza o pin*/
-  }
-} while (option !== 7);
-=======
   switch (option) {
     case 1:  
     prompt (
@@ -60,6 +28,43 @@ do {
           0. Voltar
           `
         )
+        const númerodoreceptor = prompt ("Digita um numero valido");
+    if(
+      (númerodoreceptor.length === 9 && númerodoreceptor.charAt(1)==="4") ||
+      (númerodoreceptor.length === 9 && númerodoreceptor.charAt (1) === "5")
+    ){
+    
+      const transferirvalor = (prompt ("Digita o valor a transferir"));
+  
+      if (transferirvalor){
+  
+      }
+         
+     } else {
+      saldo -= transferirvalor;
+  }
+      /*if (transferirvalor>0 && transferirvalor <= saldo){
+        alert (`transferencia de ${transferirvalor}5mt para ${númerodoreceptor} feita com sucesso`)
+    }*/
+
+    const numero = prompt ("Digita um numero valido");
+    if(
+      (numero.length === 9 && numero.charAt(1)==="2") ||
+      (numero.length === 9 && numero.charAt (1) === "3")
+    ){
+    
+      const transferirvalor = valor (prompt ("Digita o valor a transferir"));
+  
+      if (transferirvalor){
+  
+      }
+         
+     } else {
+      transferirvalor -= saldo;
+  }
+      /*if (transferirvalor>0 && transferirvalor <= saldo){
+        alert (`transferencia de ${transferirvalor}5mt para ${númerodoreceptor} feita com sucesso`)
+    }*/
       
       break;
     default:
@@ -70,4 +75,3 @@ do {
 } while (option !== 7);
 
 
->>>>>>> 712d36edde2ede170ff38b301a0c0cd1a8e0b267
