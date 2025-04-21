804bc15419a2525a42f4bd1a4753f7cca2483854
isaneyde <issitoe941@gmail.com>
2025-04-21 13:02:28 +0200

    feat: comprar sms para meu numero

--- index.js
+++ index.js
@@ -16,4 +16,60 @@ do {
     7. Sair
     `)
   );
-} while (option !== 7);
+switch (option) {
+  case 4:
+    let opcao = Number(prompt(
+      `1.Comprar para meu número
+       2.Comprar para outro número
+       3.voltar`
+    
+    ));
+    break;
+
+}
+}while(option!==7);
+
+
+
+/*switch (opcao) {
+  case 1 :
+    
+    break;
+case 3 :
+
+}*/
+function validarPin(){
+  let p1n2;
+  if(pin===pin2){
+
+  }
+}
+function ComprarmeuNumero(){
+  let pacotes=Number(prompt(`
+     1. 2MT = 100 SMS/1d
+     2. 4MT = 180 SMS/3d
+     3. 10MT = 500 SMS/7d
+     4. 20MT = 1000 SMS/7d
+     5. 40MT = 2000 SMS/30d
+     6. voltar
+     7. Menu Principal
+    `)) 
+    switch (pacotes) {
+      case 1:
+      Number(prompt("Digite o seu pin: "))
+       if(pin === pin2){
+        if (saldo>0.0){
+        return saldo-= 2;
+        }else{
+          alert (`Saldo Insuficiente, por favor faça um depósito. O seu saldo é de ${saldo} MTS.`)
+        }
+       }else{
+        alert("Pin Incorrecto,")
+       }
+        break;
+    case 2:
+        break;
+        case 3:
+          break;
+    }
+}
