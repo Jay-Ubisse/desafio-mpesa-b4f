6877b3bc2067b120de1b7f3978b131bbe17cd6dc
Cris da Silva <jnc.mataca@gmail.com>
2025-04-21 17:15:11 +0200

    a

--- index.js
+++ index.js
@@ -14,6 +14,46 @@ do {
     6. Minha conta
     7. Sair
     `)
-<<<<<<< HEAD
+
   );
+  switch(option){
+    
+    case 6:
+      menu();
+  
+      break;
+    } while (option !== 7);
+  
 } while (option !== 7);
+
+
+
+
+
+
+
+
+function menu(){
+do{
+  option=Number(   
+    prompt(` 
+    1.Verificar saldo
+    2.Alterar o pin
+    3.Sair
+    `)
+
+    );
+    switch(option){
+      case 1:
+        let nr=Number(prompt("digite o pin"));
+        
+        if(nr===pin){
+        alert(`O seu saldo é de ${saldo.toFixed(2)} MT`);
+        }else{
+          alert("pin errado")
+        }
+        break;
+
+    }
+  }while(option!==3)
+  }
