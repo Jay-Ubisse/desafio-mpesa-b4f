2300cbf766e891b8c06076ed58ac39bf2a19daa9
OlgaBernece258 <esperancamunlela@gmail.com>
2025-04-21 14:56:33 +0200

    feat: creat option mpesa

--- index.js
+++ index.js
@@ -16,4 +16,34 @@ do {
     7. Sair
     `)
   );
+
+
+  switch
+  (option){
+  
+  case 1:
+  const númerodoreceptor = prompt ("Digita um numero valido");
+    if(
+      (númerodoreceptor.length === 9 && númerodoreceptor.charAt(1)==="4") ||
+      (númerodoreceptor.length === 9 && númerodoreceptor.charAt (1) === "5")
+    ){
+    
+      const transferirvalor = Numero (prompt ("Digita o valor a transferir"));
+  
+      if (transferirvalor){
+  
+      }
+         
+     } else {
+      saldo -= transferirvalor;
+  }
+      /*if (transferirvalor>0 && transferirvalor <= saldo){
+        alert (`transferencia de ${transferirvalor}5mt para ${númerodoreceptor} feita com sucesso`)
+    }
+  
+  
+  
+  
+  //porfavor introduza o pin*/
+  }
 } while (option !== 7);
