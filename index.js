0360e0f43c823f5bd8fc5505d1c90eb3a640fb34
maudlyn01 <maudana2000@gmail.com>
2025-04-21 13:58:42 +0200

    feat: add start of transfer for BIM bank

--- index.js
+++ index.js
@@ -16,4 +16,40 @@ do {
     7. Sair
     `)
   );
+  let destino;
+  let bancaria;
+  switch (option) {
+
+    case 1: // Transferência
+      destino = Number(
+        prompt(`SEM TAXAS Transferências de qualquer valor de M-pesa para Mpesa
+          1. Carteiras Móveis
+          2. Banco
+          `));
+      break;// option
+    default: Number(prompt(`Escolha uma opção válida`));
+    break;
+      }// fim case option
+
+
+      switch (destino) {
+        case 1: //Transferir para Carteiras Moveis
+          break;
+        case 2: // Transferir para banco
+          bancaria = Number(
+            prompt(` Escolha uma opção:
+                1. Digitar NIB/ Numero de Conta
+                2. Para procurar na lista
+                `));
+          break;
+        default: Number(prompt(`Escolha uma opção válida`));
+          break;// destino
+          const isValidAccount = (nibNumber.length === 21 && [0].includes(nibNumber.charAt(0).charAt(1).charAt(2)) &&  
+          [1].includes(nibNumber.charAt(3)) || (accountNum.length===9));
+          if (isValidAccount) {
+            const value = Number(prompt("Insira o montante"));
+          }
+
+      }// fim case destino
+
 } while (option !== 7);
