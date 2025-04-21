8f1aca10933c8da1bc361b130ba9e3112996c036
Jorge Langa <jorgebernardolanga@gmail.com>
2025-04-21 17:13:13 +0200

    feat: Funcao comprarDados

--- index.js
+++ index.js
@@ -63,23 +63,27 @@ switch (Option) {
       6.Infinito
       7.ticket( Netflix, musica e Game)
       0. Voltar
-      00.Menu principal
+      00. Menu principal
+     
       `)
     );
       
-  switch (Option) {
-      case 1:  Option = Number(
-      prompt(`
-      M-pesa
    
-          1. 50MT=175MB
-          2. 15MT=75MB
-          3. 10MT=45MB
-          0.Voltar
-          00.Menu principal
-      `)
-    );
-  
+  switch (Option) {
+      case 1:  
+                
+                  Option = Number(
+                  prompt(`
+                  M-pesa
+              
+                      1. 50MT=175MB
+                      2. 15MT=75MB
+                      3. 10MT=45MB
+                      0.Voltar
+                      00.Menu principal
+                  `)
+                );
+              
           if(saldo>=50){
               let ComparePin=Number(prompt("Digite o PIN:"))
               if (pin===ComparePin) {
@@ -90,7 +94,7 @@ switch (Option) {
 
                   if(option===1){
                   
-                  alert(" O seu pedido foi submetido com sucesso")
+                    alert(` O seu pedido foi submetido com sucesso, seu novo saldo é ${saldo}`)
                     }
                     else{
                       break
@@ -106,20 +110,67 @@ switch (Option) {
           
           break;
 
-          case 2:
+          case 2: if(saldo>=15){
+            let ComparePin=Number(prompt("Digite o PIN:"))
+            if (pin===ComparePin) {
+                saldo=saldo-15
+                option=Number(prompt(`Pagar 15 MT para activar MB?
+                  1.Confirmar
+                  2.Cancelar`))
+
+                if(option===1){
+                
+                alert(` O seu pedido foi submetido com sucesso, seu novo saldo é ${saldo}`)
+                  }
+                  else{
+                    break
+                  }
+
+                
+            } else{
+             
+                alert('O seu pedido falhou!')
+            }
+
+        }
               break;
 
-          case 3: 
+          case 3:  if(saldo>=10){
+            let ComparePin=Number(prompt("Digite o PIN:"))
+            if (pin===ComparePin) {
+                saldo=saldo-10
+                option=Number(prompt(`Pagar 50 MT para activar MB?
+                  1.Confirmar
+                  2.Cancelar`))
+
+                if(option===1){
+                
+                  alert(` O seu pedido foi submetido com sucesso, seu novo saldo é ${saldo}`)
+                  }
+                  else{
+                    break
+                  }
+
+                
+            } else{
+             
+                alert('O seu pedido falhou!')
+            }
+
+        }
           break
 
-          case 4:
-              break
+        
   
-      default:
+      default: alert('A opcao digitada é invalida')
           break;
+
+         
   }
 
-  case 2:
+
+    
+  case 2://Para o outro numero
 }
 
   
