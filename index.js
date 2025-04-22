b46cfffb354b94736245270ba9fdf610febb7f3e
isaneyde <issitoe941@gmail.com>
2025-04-22 08:17:37 +0200

    update:update a function

--- index.js
+++ index.js
@@ -1,6 +1,77 @@
-let saldo = 0.0;
+let saldo = 500;
 let pin = 1234;
 let option;
+let pacotes;
+let opcao;
+function isValidatedPassword() {
+  let password = Number(prompt(" digite o pin"));
+  if (pin !== password) {
+    alert("pin errado");
+
+  } else {
+   return true;
+
+  }}
+ 
+function ComprarmeuNumero() {
+pacotes = Number(prompt(`
+1. 2MT = 100 SMS/1d
+2. 4MT = 180 SMS/3d
+3. 10MT = 500 SMS/7d
+4. 20MT = 1000 SMS/7d
+5. 40MT = 2000 SMS/30d
+6. voltar
+7. Menu Principal
+`));
+switch (pacotes) {
+  case 1:
+    isValidatedPassword();
+if (saldo > 0.0) {
+  saldo -= 2;
+  return (`PARABÉNS, comprou sms's no valor de 2MT, o teu novo saldo M-pesa é ${saldo}.`)
+} 
+
+    break;
+  case 2:
+    isValidatedPassword();
+if (saldo > 0.0) {
+  saldo -= 4;
+  return (`PARABÉNS, comprou sms's no valor de 4MT, o teu novo saldo M-pesa é ${saldo}.`)
+} 
+
+    break;
+  case 3:
+    
+  isValidatedPassword();
+  if (saldo > 0.0) {
+    saldo -= 10;
+    return (`PARABÉNS, comprou sms's no valor de 10MT, o teu novo saldo M-pesa é ${saldo}.`)
+  } 
+    break;
+  case 4:
+    isValidatedPassword();
+    if (saldo > 0.0) {
+      saldo -= 20;
+      return (`PARABÉNS, comprou sms's no valor de 20MT, o teu novo saldo M-pesa é ${saldo}.`)
+    } 
+    break;
+  case 5:
+    
+  isValidatedPassword();
+  if (saldo > 0.0) {
+    saldo -= 40;
+    return (`PARABÉNS, comprou sms's no valor de 40MT, o teu novo saldo M-pesa é ${saldo}.`)
+  }
+  break;
+  default:alert(`Saldo Insuficiente, o saldo da sua conta é ${saldo}. Obrigado!`);
+  case 6: ComprarmeuNumero();
+  break;
+}
+  }
+
+  
+ 
+
 
 do {
   option = Number(
@@ -16,99 +87,27 @@ do {
     7. Sair
     `)
   );
-} while (option !== 7);
+  
 switch (option) {
-  case 1:
-
-    break;
-  case 2:
-
-    break;
-  case 3:
-
-    break;
   case 4:
-
-    let opcao = Number(prompt(
+    opcao = Number(prompt(
       `1.Comprar para meu número
        2.Comprar para outro número
        3.voltar`
 
     ));
-    /*function validarpin() {
-      let password = Number(prompt(" digite o seu pin"));
-      if (pin !== password) {
-        alert("pin Incorrecto");
-
-      }
-    }*/
-    //ate so para chamar a funcoies
+    break;
+  }
+  
+}while (option !== 7);
     switch (opcao) {
       case 1:
-
-        function ComprarmeuNumero() {
-          let pacotes = Number(prompt(`
-     1. 2MT = 100 SMS/1d
-     2. 4MT = 180 SMS/3d
-     3. 10MT = 500 SMS/7d
-     4. 20MT = 1000 SMS/7d
-     5. 40MT = 2000 SMS/30d
-     6. voltar
-     7. Menu Principal
-    `))
-          switch (pacotes) {
-            case 1:
-              validarpin(password);
-              if (saldo > 0.0) {
-                saldo -= 2;
-                return (`PARABÉNS, comprou sms's no valor de 2MT, o saldo da sua conta é ${saldo}.`)
-              } else {
-                alert(`Saldo Insuficiente, o saldo da sua conta é ${saldo}. Obrigado!`);
-              }
-
-
-              break;
-            case 2:
-              validarpin(password);
-              if (saldo > 0.0) {
-                saldo -= 4;
-                return (`PARABÉNS, comprou sms's no valor de 2MT, o saldo da sua conta é ${saldo}.`)
-              } else {
-                alert(`Saldo Insuficiente, o saldo da sua conta é ${saldo}. Obrigado!`);
-              }
-
-              break;
-            case 3:
-              validarpin(password);
-              if (saldo > 0.0) {
-                saldo -= 10;
-                return (`PARABÉNS, comprou sms's no valor de 2MT, o saldo da sua conta é ${saldo}.`)
-              } else {
-                alert(`Saldo Insuficiente, o saldo da sua conta é ${saldo}. Obrigado!`);
-              }
-              break;
-            case 4:
-              validarpin(password);
-              if (saldo > 0.0) {
-                saldo -= 20;
-                return (`PARABÉNS, comprou sms's no valor de 2MT, o saldo da sua conta é ${saldo}.`)
-              } else {
-                alert(`Saldo Insuficiente, o saldo da sua conta é ${saldo}. Obrigado!`);
-              }
-              break;
-            case 5:
-              validarpin(password);
-              if (saldo > 0.0) {
-                saldo -= 40;
-                return (`PARABÉNS, comprou sms's no valor de 2MT, o saldo da sua conta é ${saldo}.`)
-              } else {
-                alert(`Saldo Insuficiente, o saldo da sua conta é ${saldo}. Obrigado!`);
-              }
-          }
-        }
-
+//funcao para comprar sms para meu número
+      alert(ComprarmeuNumero());  
+          
 
         break;
+        //case para comprar sms para outro numero
       case 2:
         const receberNumero = prompt("digita o numero");
         if (
@@ -118,7 +117,7 @@ switch (option) {
         ) {
           const selecionarOpcao = Number(
             prompt(
-              `1. 2mt = 200 SMS/1d
+      `1. 2mt = 200 SMS/1d
       2. 4mt = 400mt SMS/3d
       3. 10mt = 1000 SMS/7d
       4. 20mt = 2000 SMS/7d
@@ -129,25 +128,16 @@ switch (option) {
         }
         break;
       case 3:
-
+  
         break;
 
       default:
         break;
-    }
-    break;
+    
   case 5:
     alert(comprarCedelec());
-
     break;
-
-  case 6:
-
-    break;
-
-  default:
-    break;
-}
+    }
 function comprarCedelec() {
 
   option = Number(
@@ -183,17 +173,8 @@ function comprarCedelec() {
   }
 
 }
-function isValidatedPassword() {
-  let password = Number(prompt(" digite o pin"));
-  if (pin !== password) {
-    alert("pin errado");
 
-  } else {
-    let comfimar = Number(prompt(`comnfirmas a compra de CREDELECT de ${valorCompra} 1. comfimar a compra 2. sair`));
 
-  }
-
-}
 function isValidatedNumberCont() {
   let numeroComtador = Number(prompt("numero do contador"));
 
