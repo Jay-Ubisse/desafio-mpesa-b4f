9f520954e46c31a80a0baffff83039ea9ed426f6
Afonso Relane <afonso.relane@gmail.com>
2025-04-22 21:21:57 +0200

    add: function to raise money

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ do {
 
   );
   switch (option) {
+    //Opção Levantar Dinheiro
+    case 2: withdrawMoney();
     //caso a opcao seja depositar dinheiro
     case 3:depositarDinheiro();
     case 6:opcaoMinhaConta();
@@ -87,4 +89,83 @@ do{
             }
         }
     }while(option!==3)
+}
+
+function withdrawMoney() {
+  const raiseMoney = Number(
+    prompt(`
+         M-pesa
+         
+         1. No Agente
+         2. Na ATM
+         3. Voltar
+         `)
+  )
+  switch (raiseMoney) {
+    case 1: //Levantar do Agente
+      const agentNumber = prompt('Insira número do Agente')
+
+      if (agentNumber.length === 6) {
+        const amount = Number(prompt('Insira o montante para levantar'))
+        if (amount + 10 > saldo) {
+          alert('Saldo insuficiente.')
+        } else {
+          saldo -= amount
+          alert(
+            `Você levantou ${amount} MT do agente ${agentNumber}. A taxa foi de 10Mt, o seu  novo saldo é: ${
+              saldo - 10
+            } Mt`
+          )
+        }
+      } else {
+        alert('Número de agente inválido. O número deve ter 6 dígitos.')
+      }
+      break
+    case 2: // Levantar do ATM
+      const atm = Number(
+        prompt(`
+             M-pesa
+           
+             1. Ponto 24
+             2. Carteira Movel/SIMO
+             3. Voltar
+           `)
+      )
+      switch (atm) {
+        case 1:
+          const ponto24 = Number(prompt('Insira o seu PIN'))
+          if (ponto24 === pin) {
+            alert(`Insira o código ${generateRandomNumbers()} no ATM`)
+          } else {
+            alert('O PIN está incorreto')
+          }
+          break
+        case 2:
+          const simoRede = Number(prompt('Insira o seu PIN'))
+          if (simoRede === pin) {
+            alert(`Insira o código ${generateRandomNumbers()} no ATM`)
+          } else {
+            alert('O PIN está incorreto')
+          }
+          break
+        case 3:
+          break
+        default:
+          alert('Opção inválida')
+      }
+      break
+    case 3: // Voltar
+      break
+    default:
+      alert('Opção inválida')
+      break
+  }
+}
+
+function generateRandomNumbers() {
+  let numeros = ''
+  for (let i = 0; i < 6; i++) {
+    numeros += Math.floor(Math.random() * 10)
+  }
+  return numeros
 }
