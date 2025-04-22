d16b34b99541308d2d93fab1f1935b79b888f48a
Afonso Relane <afonso.relane@gmail.com>
2025-04-22 22:00:38 +0200

    add: breaks to switch-case

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let saldo = 0.0
-let pin = 1234
-let option
+let saldo = 0.0;
+let pin = 1234;
+let option;
 do {
   option = Number(
     prompt(`
@@ -14,81 +14,82 @@ do {
     6. Minha conta
     7. Sair
     `)
-
   );
   switch (option) {
     //Opção Levantar Dinheiro
-    case 2: withdrawMoney();
+    case 2:
+      withdrawMoney();
+      break;
     //caso a opcao seja depositar dinheiro
-    case 3:depositarDinheiro();
-    case 6:opcaoMinhaConta();
-      
-}} while (option !== 7);
+    case 3:
+      depositarDinheiro();
+      break;
+    case 6:
+      opcaoMinhaConta();
+      break;
+    default:
+      alert("Opção inválida");
+  }
+} while (option !== 7);
 
-
-
-
-function depositarDinheiro(){
-  let validacao = Number(prompt(`Insira o seu pin`))
+function depositarDinheiro() {
+  let validacao = Number(prompt(`Insira o seu pin`));
   while (validacao !== pin) {
-    validacao = Number(prompt(`Pin invalido, tente novamente!`))
+    validacao = Number(prompt(`Pin invalido, tente novamente!`));
   }
   let deposit = Number(
-    prompt('Depositar dinheiro \n\n Qual é o valor que deseja depositar?')
-  )
-  while ((deposit <= 0) | (deposit === '') | isNaN(deposit)) {
+    prompt("Depositar dinheiro \n\n Qual é o valor que deseja depositar?")
+  );
+  while ((deposit <= 0) | (deposit === "") | isNaN(deposit)) {
     deposit = prompt(
       `Valor invalido, Tente novamente!\n\n Insira o valor que deseja depositar`
-    )
+    );
   }
-  saldo += deposit //adicionar o valor depositado ao saldo
+  saldo += deposit; //adicionar o valor depositado ao saldo
   let out = prompt(
     `\n\n Deposito de ${deposit.toFixed(
       2
     )} MT concluido com sucesso! \n\n 1.Continuar \n 2.Sair`
-  )
+  );
   if (out == 2) {
-    option = 7
+    option = 7;
   } //opcao para sair do programa
-  }
+}
 
-
-
-function opcaoMinhaConta(){
-do{
-  option=Number(   
-    prompt(` 
+function opcaoMinhaConta() {
+  do {
+    option = Number(
+      prompt(` 
     1.Verificar saldo
     2.Alterar o pin
     3.Sair
     `)
-
     );
-    switch(option){
+    switch (option) {
       case 1:
-        let nr=Number(prompt("digite o pin"));
-        
-        if(nr===pin){
-        alert(`O seu saldo é de ${saldo.toFixed(2)} MT`);
-        }else{
-          alert("pin errado")
+        let nr = Number(prompt("digite o pin"));
+
+        if (nr === pin) {
+          alert(`O seu saldo é de ${saldo.toFixed(2)} MT`);
+        } else {
+          alert("pin errado");
         }
         break;
-          case 2:
-            const lastPIN = Number(prompt("Insira o pin anterior"));
-            if (lastPIN === pin) {
-              const newPIN = Number(prompt("Insira o novo pin"));
-              if (newPIN !== pin) {
-                alert("PIN alterado com sucesso");
-                pin = newPIN;
-              } else {
-                alert("O pin Actual deve ser diferente do Antigo");
-              }
-            } else {
-              alert("PIN incorreto, tente novamente");
-            }
+      case 2:
+        const lastPIN = Number(prompt("Insira o pin anterior"));
+        if (lastPIN === pin) {
+          const newPIN = Number(prompt("Insira o novo pin"));
+          if (newPIN !== pin) {
+            alert("PIN alterado com sucesso");
+            pin = newPIN;
+          } else {
+            alert("O pin Actual deve ser diferente do Antigo");
+          }
+        } else {
+          alert("PIN incorreto, tente novamente");
         }
-    }while(option!==3)
+    }
+  } while (option !== 3);
 }
 
 function withdrawMoney() {
@@ -100,27 +101,27 @@ function withdrawMoney() {
          2. Na ATM
          3. Voltar
          `)
-  )
+  );
   switch (raiseMoney) {
     case 1: //Levantar do Agente
-      const agentNumber = prompt('Insira número do Agente')
+      const agentNumber = prompt("Insira número do Agente");
 
       if (agentNumber.length === 6) {
-        const amount = Number(prompt('Insira o montante para levantar'))
+        const amount = Number(prompt("Insira o montante para levantar"));
         if (amount + 10 > saldo) {
-          alert('Saldo insuficiente.')
+          alert("Saldo insuficiente.");
         } else {
-          saldo -= amount
+          saldo -= amount;
           alert(
             `Você levantou ${amount} MT do agente ${agentNumber}. A taxa foi de 10Mt, o seu  novo saldo é: ${
               saldo - 10
             } Mt`
-          )
+          );
         }
       } else {
-        alert('Número de agente inválido. O número deve ter 6 dígitos.')
+        alert("Número de agente inválido. O número deve ter 6 dígitos.");
       }
-      break
+      break;
     case 2: // Levantar do ATM
       const atm = Number(
         prompt(`
@@ -130,42 +131,42 @@ function withdrawMoney() {
              2. Carteira Movel/SIMO
              3. Voltar
            `)
-      )
+      );
       switch (atm) {
         case 1:
-          const ponto24 = Number(prompt('Insira o seu PIN'))
+          const ponto24 = Number(prompt("Insira o seu PIN"));
           if (ponto24 === pin) {
-            alert(`Insira o código ${generateRandomNumbers()} no ATM`)
+            alert(`Insira o código ${generateRandomNumbers()} no ATM`);
           } else {
-            alert('O PIN está incorreto')
+            alert("O PIN está incorreto");
           }
-          break
+          break;
         case 2:
-          const simoRede = Number(prompt('Insira o seu PIN'))
+          const simoRede = Number(prompt("Insira o seu PIN"));
           if (simoRede === pin) {
-            alert(`Insira o código ${generateRandomNumbers()} no ATM`)
+            alert(`Insira o código ${generateRandomNumbers()} no ATM`);
           } else {
-            alert('O PIN está incorreto')
+            alert("O PIN está incorreto");
           }
-          break
+          break;
         case 3:
-          break
+          break;
         default:
-          alert('Opção inválida')
+          alert("Opção inválida");
       }
-      break
+      break;
     case 3: // Voltar
-      break
+      break;
     default:
-      alert('Opção inválida')
-      break
+      alert("Opção inválida");
+      break;
   }
 }
 
 function generateRandomNumbers() {
-  let numeros = ''
+  let numeros = "";
   for (let i = 0; i < 6; i++) {
-    numeros += Math.floor(Math.random() * 10)
+    numeros += Math.floor(Math.random() * 10);
   }
-  return numeros
+  return numeros;
 }
